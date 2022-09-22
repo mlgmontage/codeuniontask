4a586ca6aba07965100a19285dd7dd933bd9c48c
abay <a.abay@mybuh.kz>
2022-09-22 09:57:44 +0600

    fix: Main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Jumbotron from "./components/Jumbotron";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <div className="text-red-500">
       <Header />
       <Jumbotron />
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="w-[1240px] m-auto py-[60px] text-dark">
+      <h2 className="text-xl">Популярные предложения</h2>
+      <div className="text-base mb-[28px]">
+        Предложения, которые любят наши клиенты
+      </div>
+    </div>
+  );
+};
+
+export default Main;
